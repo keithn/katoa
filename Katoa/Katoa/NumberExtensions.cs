1ff9624e7652b1904b8b1db965d576f284bda3ba
keith.nicholas <keith.nicholas@gmail.com>
2018-12-15 12:53:52 +1300

    Updated comment about Number extensions as they are tricky to do generically

--- Katoa/Katoa/NumberExtensions.cs
+++ Katoa/Katoa/NumberExtensions.cs
@@ -11,8 +11,9 @@ namespace Katoa
         /// <param name="n"></param>
         /// <returns></returns>
         public static int[] ToDigits<T>(this T n)
-            where T : struct   // struct is unfortunate as there is no good way to identify numbers
+            where T : struct   // struct is unfortunate as there is no good way to identify numbers, but this means it won't turn up on most types
                                // see https://stackoverflow.com/questions/32664/is-there-a-constraint-that-restricts-my-generic-method-to-numeric-types
+                               // An alternative is to make an extension per type all calling this common base.
         {
             if(!n.GetType().IsNumeric()) return new int[0];
             return n.ToString().Select(digit => int.Parse(digit.ToString())).ToArray();
