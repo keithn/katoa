f817b7d9d97e5cc7b0560f777a6c935db381affb
keith.nicholas <keith.nicholas@gmail.com>
2018-12-15 13:51:38 +1300

    Added MatchesFrom which compares two collections starting at an offset to see if they match.

--- Katoa/Katoa.Tests/ListHelperExtensionTests.cs
+++ Katoa/Katoa.Tests/ListHelperExtensionTests.cs
@@ -15,15 +15,26 @@ namespace Katoa.Tests
             Assert.Equal(4, min);
             Assert.Equal(8, max);
         }
+
         [Fact]
         public void MinMaxWithSelectors()
         {
-            var list = new List<int[]>() {new[] {4, 14}, new[] {5, 15}, new[] {6, 16}};
+            var list = new List<int[]> {new[] {4, 14}, new[] {5, 15}, new[] {6, 16}};
 
             var (min, max) = list.MinMax(l => l[0], l => l[1]);
 
             Assert.Equal(4, min);
             Assert.Equal(16, max);
+        }
+
+
+        [Fact]
+        public void MatchesFrom()
+        {
+            Assert.True(new[]{1,2,3,4,5,6,7}.MatchesFrom(2, new[]{3,4,5}));
+            Assert.False(new[]{1,2,3,4,1,6,7}.MatchesFrom(2, new[]{3,4,5}));
+            Assert.True(new[]{1,2,3,4,1,6,7}.MatchesFrom(2, new[]{3,4,5}, 0, 2));
+            Assert.True(new[]{1,2,3,4,1,6,7}.MatchesFrom(-2, new[]{6,7}));
         }
 
     }
--- Katoa/Katoa/EnumerableHelperExtension.cs
+++ Katoa/Katoa/EnumerableHelperExtension.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace Katoa
 {
@@ -16,5 +17,32 @@ namespace Katoa
             if (maxSelect == null) maxSelect = minSelect;
             return (c.Min(minSelect), c.Max(maxSelect));
         }
+
+        /// <summary>
+        /// Do the collections Match from a specific location in each collection, supports negative offset to index from the end
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="c"></param>
+        /// <param name="offset"></param>
+        /// <param name="other"></param>
+        /// <param name="otherOffset"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public static bool MatchesFrom<T>(this ICollection<T> c, int offset, ICollection<T> other, int otherOffset =0, int? count = null)
+         where T : IComparable
+        {
+            if (offset < 0) offset = c.Count + offset;
+            if (otherOffset < 0) otherOffset = other.Count + otherOffset;
+            if (count == null) count = other.Count - otherOffset;
+            if (other.Count < otherOffset+count) return false;
+            if (c.Count < offset + count) return false;
+            for (int i = 0; i < count; i++)
+            {
+                if (c.ElementAt(i + offset).CompareTo(other.ElementAt(i)) != 0) return false;
+            }
+            return true;
+        }
+
+
     }
 }
