3f70e301daa81be94cbd0674efeda4e56edfb9e2
Jonathan <vanillajonathan@users.noreply.github.com>
2022-07-11 16:39:16 +0200

    Update Ascii.cs

--- Katoa/Katoa/Ascii.cs
+++ Katoa/Katoa/Ascii.cs
@@ -4,9 +4,10 @@ namespace Katoa
 {
     public static class Ascii
     {
+        public static string Alphanumeric = Letters + Digits;
         public static string Lower = "abcdefghijklmnopqrstuvwxyz";
-        public static string Upper = Lower.ToUpper();
-        public static string Letters = Lower+Upper;
+        public static string Upper = Lower.ToUpperInvariant();
+        public static string Letters = Lower + Upper;
         public static string Digits = "0123456789";
     }
 }
